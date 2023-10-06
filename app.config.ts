a3be2dc2c31ac92ae1693a4d3536259ed5b0f5dd
Neomo <toomy12@qq.com>
2023-10-06 15:45:56 +0800

    Up

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'Neo',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
